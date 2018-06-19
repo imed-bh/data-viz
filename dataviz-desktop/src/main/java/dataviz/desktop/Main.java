d760da0f5e9b278062c3e9ad23bce78bb21f240e
Imed Ben Heni <ibenheni@ecovadis.com>
2018-06-19 12:53:10 +0100

    gradle multi module configuration

---
+++ dataviz-desktop/src/main/java/dataviz/desktop/Main.java
@@ -0,0 +1,10 @@
+package dataviz.desktop;
+
+import dataviz.struct.Dummy;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello DataViz: " + new Dummy().doNothing());
+    }
+}
